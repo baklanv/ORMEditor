82d0728d3c7ebbca5052e0f864e735530666afee
baklanv <63604180+baklanv@users.noreply.github.com>
2021-06-11 14:01:00 +0300

    Исправлен стиль и добавлены методы, меняющие внешний вид ограничений

--- src/ORM_Presenter/ExclusiveOrConstraintPresenter.java
+++ src/ORM_Presenter/ExclusiveOrConstraintPresenter.java
@@ -2,35 +2,43 @@ package ORM_Presenter;
 
 import com.mxgraph.model.mxCell;
 import org.jetbrains.annotations.NotNull;
+import org.vstu.nodelinkdiagram.statuses.ValidateStatus;
 import org.vstu.nodelinkdiagram.util.Point;
+import org.vstu.orm2diagram.model.ORM_ExclusionOrConstraint;
 
-public class ExclusiveOrConstraintPresenter extends ElementPresenter{
-    public ExclusiveOrConstraintPresenter(@NotNull GraphPresenter graphPresenter, @NotNull Point pos) {
+public class ExclusiveOrConstraintPresenter extends ElementPresenter {
+    String _style = "shape=ellipse;perimeter=ellipsePerimeter;spacing=10;strokeColor=purple;foldable=false;editable=0;resizable=false;";
+
+    public ExclusiveOrConstraintPresenter(@NotNull GraphPresenter graphPresenter, @NotNull Point pos, @NotNull ORM_ExclusionOrConstraint orm_exclusionOrConstraint) {
         super(graphPresenter);
 
         _mxCell = (mxCell) graphPresenter.getMxGraph().insertVertex(graphPresenter.getMxGraph().getDefaultParent(),
-                null, "", pos.getX(), pos.getY(), 26, 26,
-                "shape=ellipse;perimeter=ellipsePerimeter;spacing=10;" +
-                        "strokeColor=purple;foldable=false;" +
-                        "editable=0;resizable=false");
-        //_mxCell.setConnectable(true);
-        mxCell _mxCell1 = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCell,
-                null, "", 8, 8, 10, 10,
-                "shape=ellipse;perimeter=ellipsePerimeter;spacing=10;" +
-                        "resizable=false;strokeColor=purple;fillColor=purple;" +
+                null, "", pos.getX(), pos.getY(), 30, 30, _style);
+
+        mxCell mxCell = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCell, null, "", 10, 10, 10, 10,
+                "shape=ellipse;spacing=10;resizable=false;strokeColor=purple;fillColor=purple;" +
                         "editable=0;movable=false;connectable=false;");
-        _mxCell1.setConnectable(false);
-        mxCell line1 = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCell,
-                null, "", 0, 8, 26, 10,
-                "shape=line;perimeter=linePerimeter;spacing=10;" +
-                        "resizable=false;strokeColor=purple;fillColor=purple;" +
+        mxCell.setConnectable(false);
+        mxCell line1 = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCell, null, "", 0, 10, 30, 10,
+                "shape=line;perimeter=linePerimeter;spacing=10;resizable=false;strokeColor=purple;fillColor=purple;" +
                         "editable=0;movable=false;connectable=false;rotation=45");
         line1.setConnectable(false);
-        mxCell line2 = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCell,
-                null, "", 0, 8, 26, 10,
-                "shape=line;perimeter=linePerimeter;spacing=10;" +
-                        "resizable=false;strokeColor=purple;fillColor=purple;" +
+        mxCell line2 = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCell, null, "", 0, 10, 30, 10,
+                "shape=line;perimeter=linePerimeter;spacing=10;resizable=false;strokeColor=purple;fillColor=purple;" +
                         "editable=0;movable=false;connectable=false;rotation=135");
         line2.setConnectable(false);
+
+        _diagramElement = orm_exclusionOrConstraint;
+        _validateStatus = ValidateStatus.Acceptable;
+    }
+
+    public void fail() {
+        _graphPresenter.getMxGraph().setCellStyle(_style + "strokeColor=red", new Object[]{_mxCell});
+        _validateStatus = ValidateStatus.Invalid;
+    }
+
+    public void success() {
+        _graphPresenter.getMxGraph().setCellStyle(_style, new Object[]{_mxCell});
+        _validateStatus = ValidateStatus.Acceptable;
     }
 }
--- src/ORM_Presenter/InclusiveOrConstraintPresenter.java
+++ src/ORM_Presenter/InclusiveOrConstraintPresenter.java
@@ -2,26 +2,35 @@ package ORM_Presenter;
 
 import com.mxgraph.model.mxCell;
 import org.jetbrains.annotations.NotNull;
+import org.vstu.nodelinkdiagram.statuses.ValidateStatus;
 import org.vstu.nodelinkdiagram.util.Point;
-
+import org.vstu.orm2diagram.model.ORM_InclusiveOrConstraint;
 
 public class InclusiveOrConstraintPresenter extends ElementPresenter {
-    public InclusiveOrConstraintPresenter(@NotNull GraphPresenter graphPresenter, @NotNull Point pos) {
+    String _style = "shape=ellipse;perimeter=ellipsePerimeter;spacing=10;strokeColor=purple;foldable=false;editable=0;resizable=false;";
+
+    public InclusiveOrConstraintPresenter(@NotNull GraphPresenter graphPresenter, @NotNull Point pos, @NotNull ORM_InclusiveOrConstraint orm_inclusiveOrConstraint) {
         super(graphPresenter);
 
         _mxCell = (mxCell) graphPresenter.getMxGraph().insertVertex(graphPresenter.getMxGraph().getDefaultParent(),
-                null, "", pos.getX(), pos.getY(), 26, 26,
-                "shape=ellipse;perimeter=ellipsePerimeter;spacing=10;" +
-                        "strokeColor=purple;foldable=false;" +
-                        "editable=0;resizable=false");
-        //_mxCell.setConnectable(true);
-        mxCell _mxCell1 = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCell,
-                null, "", 8, 8, 10, 10,
-                "shape=ellipse;perimeter=ellipsePerimeter;spacing=10;" +
-                        "resizable=false;strokeColor=purple;fillColor=purple;" +
+                null, "", pos.getX(), pos.getY(), 30, 30, _style);
+
+        mxCell _mxCell1 = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCell, null, "", 10, 10, 10, 10,
+                "shape=ellipse;perimeter=ellipsePerimeter;spacing=10;resizable=false;strokeColor=purple;fillColor=purple;" +
                         "editable=0;movable=false;connectable=false;");
         _mxCell1.setConnectable(false);
 
+        _diagramElement = orm_inclusiveOrConstraint;
+        _validateStatus = ValidateStatus.Acceptable;
+    }
 
+    public void fail() {
+        _graphPresenter.getMxGraph().setCellStyle(_style + "strokeColor=red", new Object[]{_mxCell});
+        _validateStatus = ValidateStatus.Invalid;
+    }
+
+    public void success() {
+        _graphPresenter.getMxGraph().setCellStyle(_style, new Object[]{_mxCell});
+        _validateStatus = ValidateStatus.Acceptable;
     }
 }
