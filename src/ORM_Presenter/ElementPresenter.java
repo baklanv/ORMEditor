c3ddadc40d0ce1e7c6317808a7289ec09be6ca4a
baklanv <63604180+baklanv@users.noreply.github.com>
2021-06-11 14:12:34 +0300

    Добавлен статус для валидации

--- src/ORM_Presenter/ElementPresenter.java
+++ src/ORM_Presenter/ElementPresenter.java
@@ -3,33 +3,35 @@ package ORM_Presenter;
 import com.mxgraph.model.mxCell;
 import org.jetbrains.annotations.NotNull;
 import org.vstu.nodelinkdiagram.DiagramElement;
+import org.vstu.nodelinkdiagram.statuses.ValidateStatus;
 
 public abstract class ElementPresenter {
     protected mxCell _mxCell;
     protected DiagramElement _diagramElement;
     protected GraphPresenter _graphPresenter;
+    protected ValidateStatus _validateStatus;
 
-    public ElementPresenter(@NotNull GraphPresenter graphPresenter){
-        _graphPresenter =  graphPresenter;
+    public ElementPresenter(@NotNull GraphPresenter graphPresenter) {
+        _graphPresenter = graphPresenter;
     }
 
-    protected GraphPresenter getGraphPresenter(){
+    protected GraphPresenter getGraphPresenter() {
         return _graphPresenter;
     }
 
-    void setORM_Element(@NotNull DiagramElement diagramElement){
-        _diagramElement = diagramElement;
-    }
-
-    public DiagramElement getORM_Element(){
+    public DiagramElement getORM_Element() {
         return _diagramElement;
     }
 
-    public mxCell get_mxCell(){
+    public mxCell get_mxCell() {
         return _mxCell;
     }
 
-    void destroy(){
-        getGraphPresenter().getMxGraph().removeCells(new Object[]{_mxCell}, true);
+    void destroy() {
+        getGraphPresenter().getMxGraph().removeCells(new Object[]{_mxCell}, false);
+    }
+
+    public ValidateStatus getValidateStatus() {
+        return _validateStatus;
     }
 }
