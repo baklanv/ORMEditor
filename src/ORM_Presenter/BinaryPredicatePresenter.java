90ef84e56193895db27ff752bb55b28c67036567
baklanv <63604180+baklanv@users.noreply.github.com>
2021-06-11 14:11:52 +0300

    Добавлена связь с RolePresenter

--- src/ORM_Presenter/BinaryPredicatePresenter.java
+++ src/ORM_Presenter/BinaryPredicatePresenter.java
@@ -2,28 +2,62 @@ package ORM_Presenter;
 
 import com.mxgraph.model.mxCell;
 import org.jetbrains.annotations.NotNull;
+import org.vstu.nodelinkdiagram.statuses.ValidateStatus;
 import org.vstu.nodelinkdiagram.util.Point;
+import org.vstu.orm2diagram.model.ORM_BinaryPredicate;
 
-public class BinaryPredicatePresenter extends ElementPresenter{
-    mxCell _mxCellRole;
-    mxCell _mxCellInverseRole;
-    mxCell _mxCellParent;
+public class BinaryPredicatePresenter extends ElementPresenter {
+    private String _style = "deletable=false;verticalLabelPosition=top;movable=false;resizable=false;";
+    protected RolePresenter _role;
+    protected RolePresenter _inverseRole;
+    protected mxCell _mxCellParent;
 
-    public BinaryPredicatePresenter(@NotNull GraphPresenter graphPresenter,  @NotNull Point pos) {
+    public BinaryPredicatePresenter(@NotNull GraphPresenter graphPresenter, @NotNull Point pos, @NotNull ORM_BinaryPredicate orm_binaryPredicate) {
         super(graphPresenter);
 
         // _mxCell - _mxCellGrandParent
-        _mxCell = (mxCell) graphPresenter.getMxGraph().insertVertex(graphPresenter.getMxGraph().getDefaultParent(),
-                null, "",  pos.getX(), pos.getY(), 66, 26,
-                "opacity=0;connectable=false;foldable=false;verticalLabelPosition=top;resizable=false;");
+        _mxCell = (mxCell) graphPresenter.getMxGraph().insertVertex(graphPresenter.getMxGraph().getDefaultParent(), null, "",
+                pos.getX(), pos.getY(), 70, 30, "opacity=0;connectable=false;foldable=false;verticalLabelPosition=top;resizable=false;editable=0;");
         _mxCell.setConnectable(false);
 
-        _mxCellParent = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCell, null, "", 3, 3, 60, 20, "opacity=0;connectable=false;verticalLabelPosition=top;deletable=false;foldable=false;resizable=false;movable=false");
+        _mxCellParent = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCell, null, "", 5, 5, 60, 20,
+                "opacity=0;connectable=false;verticalLabelPosition=top;deletable=false;foldable=false;resizable=false;movable=false;editable=0;");
         _mxCellParent.setConnectable(false);
 
-        _mxCellRole = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCellParent, null, "", 0, 0, 30, 20, "editable=0;deletable=false;verticalLabelPosition=top;movable=false;resizable=false");
+        // создание левой роли
+        mxCell mxCellRole = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCellParent, null, "", 0, 0, 30, 20,
+                _style);
 
-        _mxCellInverseRole = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCellParent, null, "", 30, 0, 30, 20, "editable=0;deletable=false;verticalLabelPosition=top;movable=false;resizable=false");
+        // создаем левый RolePresenter
+        _role = new RolePresenter(graphPresenter, mxCellRole, orm_binaryPredicate.getItem(0));
 
+        //создание правой роли
+        mxCell mxCellInverseRole = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCellParent, null, "", 30, 0, 30, 20,
+                _style);
+
+        // создаем правый RolePresenter
+        _inverseRole = new RolePresenter(graphPresenter, mxCellInverseRole, orm_binaryPredicate.getItem(1));
+
+        _diagramElement = orm_binaryPredicate;
+        ((ORM_BinaryPredicate) _diagramElement).setPosition(pos);
+        _validateStatus = ValidateStatus.Acceptable;
+    }
+
+    public RolePresenter getRole() {
+        return _role;
+    }
+
+    public RolePresenter getInverseRole() {
+        return _inverseRole;
+    }
+
+    public void fail() {
+        _graphPresenter.getMxGraph().setCellStyle(_style + "strokeColor=red", new Object[]{_role.get_mxCell(), _inverseRole.get_mxCell()});
+        _validateStatus = ValidateStatus.Invalid;
+    }
+
+    public void success() {
+        _graphPresenter.getMxGraph().setCellStyle(_style, new Object[]{_role.get_mxCell(), _inverseRole.get_mxCell()});
+        _validateStatus = ValidateStatus.Acceptable;
     }
 }
--- src/ORM_Presenter/UnaryPredicatePresenter.java
+++ src/ORM_Presenter/UnaryPredicatePresenter.java
@@ -2,14 +2,43 @@ package ORM_Presenter;
 
 import com.mxgraph.model.mxCell;
 import org.jetbrains.annotations.NotNull;
+import org.vstu.nodelinkdiagram.statuses.ValidateStatus;
 import org.vstu.nodelinkdiagram.util.Point;
+import org.vstu.orm2diagram.model.ORM_UnaryPredicate;
 
-public class UnaryPredicatePresenter extends ElementPresenter{
-    public UnaryPredicatePresenter(@NotNull GraphPresenter graphPresenter, @NotNull Point pos) {
+public class UnaryPredicatePresenter extends ElementPresenter {
+    private String _style = "verticalLabelPosition=top;resizable=false;movable=false;";
+    protected RolePresenter _role;
+
+    public UnaryPredicatePresenter(@NotNull GraphPresenter graphPresenter, @NotNull Point pos, @NotNull ORM_UnaryPredicate orm_unaryPredicate) {
         super(graphPresenter);
 
         _mxCell = (mxCell) graphPresenter.getMxGraph().insertVertex(graphPresenter.getMxGraph().getDefaultParent(),
-                null, "", pos.getX(), pos.getY(), 30, 20,
-                "verticalLabelPosition=top;resizable=false");
+                null, "", pos.getX(), pos.getY(), 40, 30,
+                "verticalLabelPosition=top;resizable=false;foldable=false;opacity=1;editable=false;");
+
+        //создание роли
+        mxCell mxCellRole = (mxCell) graphPresenter.getMxGraph().insertVertex(_mxCell, null, "", 5, 5, 30, 20, _style);
+        _role = new RolePresenter(graphPresenter, mxCellRole, orm_unaryPredicate.getItem(0));
+
+
+        _diagramElement = orm_unaryPredicate;
+        ((ORM_UnaryPredicate) _diagramElement).setPosition(pos);
+        _validateStatus = ValidateStatus.Acceptable;
+    }
+
+    // -------------- Характеристики представления для Value Type -----------
+    public RolePresenter getRole() {
+        return _role;
+    }
+
+    public void fail() {
+        _graphPresenter.getMxGraph().setCellStyle(_style + "strokeColor=red", new Object[]{_role.get_mxCell()});
+        _validateStatus = ValidateStatus.Invalid;
+    }
+
+    public void success() {
+        _graphPresenter.getMxGraph().setCellStyle(_style, new Object[]{_role.get_mxCell()});
+        _validateStatus = ValidateStatus.Acceptable;
     }
 }
