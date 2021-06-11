47905f5ab750dacebd74fbb782615d84e2c6af48
baklanv <63604180+baklanv@users.noreply.github.com>
2021-06-11 14:29:05 +0300

    Добавлен обработчик события isUpdate, исправлено форматирование кода

--- src/ORM2Editor/ORM2Editor.java
+++ src/ORM2Editor/ORM2Editor.java
@@ -4,14 +4,16 @@ import ORM_Event.ORM_EventObject;
 import ORM_Presenter.GraphPresenter;
 import com.mxgraph.model.mxCell;
 import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.swing.util.mxSwingConstants;
 import com.mxgraph.util.mxEvent;
 import com.mxgraph.util.mxEventObject;
 import com.mxgraph.util.mxEventSource;
 import com.mxgraph.view.mxGraphSelectionModel;
 import com.mxgraph.view.mxStylesheet;
 import org.vstu.nodelinkdiagram.*;
+import org.vstu.nodelinkdiagram.statuses.ValidateStatus;
 import org.vstu.nodelinkdiagram.util.Point;
-import org.vstu.orm2diagram.model.ORM_DiagramFactory;
+import org.vstu.orm2diagram.model.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,9 +21,9 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ORM2Editor extends JFrame implements DiagramClient {
 
@@ -51,15 +53,19 @@ public class ORM2Editor extends JFrame implements DiagramClient {
         _graph = new ORM2Editor_mxGraph(_graphPresenter);
         _graphComponent = new mxGraphComponent(_graph);
         _graphPresenter = new GraphPresenter(_graph, _clientDiagramModel, _graphComponent);
+        _clientDiagramModel.addListener(new ModelUpdateListener());
+
+
+        mxSwingConstants.VERTEX_SELECTION_STROKE = new BasicStroke(1.0F, 0, 0, 10.0F, new float[]{7.0F, 7.0F}, 0.0F);
+        mxSwingConstants.EDGE_SELECTION_STROKE = new BasicStroke(1.0F, 0, 0, 10.0F, new float[]{7.0F, 7.0F}, 0.0F);
 
 
         ORM_mxMultiplicity[] _mxMultiplicity = new ORM_mxMultiplicity[1];
-
         _mxMultiplicity[0] = new ORM_mxMultiplicity(_graphPresenter);
 
         _graph.setMultiplicities(_mxMultiplicity);
 
-        _graph.setAllowDanglingEdges(false);
+        _graph.setAllowDanglingEdges(false); // запещаем висячие
         _graph.setAllowLoops(false);
 
         _graphComponent.setConnectable(false);
@@ -68,7 +74,6 @@ public class ORM2Editor extends JFrame implements DiagramClient {
         mxStylesheet _styleSheet = _graph.getStylesheet();
         Map<String, Object> _defaultEdgeStyle = _styleSheet.getDefaultEdgeStyle();
         Map<String, Object> _defaultVertexStyle = _styleSheet.getDefaultVertexStyle();
-        _defaultEdgeStyle.put("editable", "0");
         _defaultEdgeStyle.put("endArrow", " ");
         _defaultEdgeStyle.put("strokeColor", "black");
         _defaultVertexStyle.put("fontSize", 12); //ORM2 default size is 7, in here it is so small
@@ -77,7 +82,6 @@ public class ORM2Editor extends JFrame implements DiagramClient {
         _defaultVertexStyle.put("strokeColor", "black");
         _defaultVertexStyle.put("spacing", 10);
         _defaultVertexStyle.put("spacingTop", 3);
-        _defaultVertexStyle.put("autosize", 1);
 
         getContentPane().add(_graphComponent);
 
@@ -108,7 +112,7 @@ public class ORM2Editor extends JFrame implements DiagramClient {
             if (cell != null) {
                 ORM_EventObject ormEventObject = new ORM_EventObject(this);
                 ormEventObject.set_mxCell(cell);
-                _graph.fireprocessOfCreatingConstrainAssociation(ormEventObject);
+                _graph.fireProcessOfCreatingConstrainAssociation(ormEventObject);
 
                 _graphComponent.setConnectable(false);
             }
@@ -119,62 +123,63 @@ public class ORM2Editor extends JFrame implements DiagramClient {
             public void mouseReleased(MouseEvent e) {
                 _clientDiagramModel.beginUpdate();
                 switch (_activeAction) {
-
-                    case ENTITY_TYPE:
+                    case ENTITY_TYPE -> {
                         _graphPresenter.createEntityTypePresenter(new Point(e.getX(), e.getY()));
                         ((JToggleButton) _tb.getComponent(ENTITY_TYPE)).setSelected(false);
                         _activeAction = -1;
-                        break;
-                    case VALUE_TYPE:
+                    }
+                    case VALUE_TYPE -> {
                         _graphPresenter.createValueTypePresenter(new Point(e.getX(), e.getY()));
                         ((JToggleButton) _tb.getComponent(VALUE_TYPE)).setSelected(false);
                         _activeAction = -1;
-                        break;
-                    case UNARY_FACT:
+                    }
+                    case UNARY_FACT -> {
                         _graphPresenter.createUnaryPredicatePresenter(new Point(e.getX(), e.getY()));
                         ((JToggleButton) _tb.getComponent(UNARY_FACT)).setSelected(false);
                         _activeAction = -1;
-                        break;
-                    case BINARY_FACT:
+                    }
+                    case BINARY_FACT -> {
                         _graphPresenter.createBinaryPredicatePresenter(new Point(e.getX(), e.getY()));
                         ((JToggleButton) _tb.getComponent(BINARY_FACT)).setSelected(false);
                         _activeAction = -1;
-                        break;
-                    case EXCLUSION_CONSTRAINT:
+                    }
+                    case EXCLUSION_CONSTRAINT -> {
                         _graphPresenter.createExclusionConstraintPresenter(new Point(e.getX(), e.getY()));
                         ((JToggleButton) _tb.getComponent(EXCLUSION_CONSTRAINT)).setSelected(false);
                         _activeAction = -1;
-                        break;
-                    case INCLUSIVE_OR_CONSTRAINT:
+                    }
+                    case INCLUSIVE_OR_CONSTRAINT -> {
                         _graphPresenter.createInclusiveOrConstraintPresenter(new Point(e.getX(), e.getY()));
                         ((JToggleButton) _tb.getComponent(INCLUSIVE_OR_CONSTRAINT)).setSelected(false);
                         _activeAction = -1;
-                        break;
-                    case EXCLUSIVE_OR_CONSTRAINT:
+                    }
+                    case EXCLUSIVE_OR_CONSTRAINT -> {
                         _graphPresenter.createExclusiveOrConstraintPresenter(new Point(e.getX(), e.getY()));
                         ((JToggleButton) _tb.getComponent(INCLUSIVE_OR_CONSTRAINT)).setSelected(false);
                         _activeAction = -1;
-                        break;
-                    case ROLE_ASSOC:
+                    }
+                    case ROLE_ASSOC -> {
                         ((JToggleButton) _tb.getComponent(ROLE_ASSOC)).setSelected(false);
                         _activeAction = -1;
-                        break;
-                    case SUBTYPING:
+                        _graphPresenter.endCreatingEdgeBy_mxGraph();
+                    }
+                    case SUBTYPING -> {
                         ((JToggleButton) _tb.getComponent(SUBTYPING)).setSelected(false);
                         _activeAction = -1;
-                        break;
-                    case CONSTRAIN_ASSOCIATION:
+                        _graphPresenter.endCreatingEdgeBy_mxGraph();
+                    }
+                    case CONSTRAIN_ASSOCIATION -> {
                         ((JToggleButton) _tb.getComponent(CONSTRAIN_ASSOCIATION)).setSelected(false);
                         _activeAction = -1;
-                        break;
-                    case DELETE:
+                    }
+                    case DELETE -> {
                         mxCell deleteCell = (mxCell) _graph.getSelectionCell();
                         if (deleteCell != null) {
                             _graphPresenter.deleteElementPresenter(deleteCell);
                         }
                         ((JToggleButton) _tb.getComponent(DELETE)).setSelected(false);
                         _activeAction = -1;
-                        break;
+                    }
                 }
             }
         });
@@ -334,6 +339,123 @@ public class ORM2Editor extends JFrame implements DiagramClient {
         }
     }
 
+    private class ModelUpdateListener implements ClientDiagramModelListener {
+        @Override
+        public void isUpdated(ModelUpdateEvent modelUpdateEvent) {
+            updateEntityTypePresenter();
+            updateValueTypePresenter();
+            updateUnaryPredicatePresenter();
+            updateBinaryPredicatePresenter();
+            updateInclusiveOrConstraintPresenter();
+            updateExclusionConstraintPresenter();
+            updateExclusiveOrConstraintPresenter();
+        }
+
+        private void updateExclusionConstraintPresenter() {
+            // РЕДАКТИРОВАНИЕ ORM_ExclusionConstraint
+            List<ORM_ExclusionConstraint> ormExclusionConstraints = _clientDiagramModel.getElements(ORM_ExclusionConstraint.class).
+                    filter(s -> s.getValidateStatus() == ValidateStatus.Intermediate).collect(Collectors.toList());
+            for (ORM_ExclusionConstraint element : ormExclusionConstraints) {
+                _graphPresenter.changeState(element, ValidateStatus.Invalid);
+            }
+            List<ORM_ExclusionConstraint> exclusionConstraintList = _clientDiagramModel.getElements(ORM_ExclusionConstraint.class).
+                    filter(e -> _graphPresenter.getElementPresenter(e).getValidateStatus() == ValidateStatus.Invalid
+                            && e.getValidateStatus() == ValidateStatus.Acceptable).collect(Collectors.toList());
+            for (ORM_ExclusionConstraint element : exclusionConstraintList) {
+                _graphPresenter.changeState(element, ValidateStatus.Acceptable);
+            }
+        }
+
+        private void updateExclusiveOrConstraintPresenter() {
+            // РЕДАКТИРОВАНИЕ ORM_InclusiveOrConstraint
+            List<ORM_ExclusionOrConstraint> ormExclusionOrConstraints = _clientDiagramModel.getElements(ORM_ExclusionOrConstraint.class).
+                    filter(s -> s.getValidateStatus() == ValidateStatus.Intermediate).collect(Collectors.toList());
+            for (ORM_ExclusionOrConstraint element : ormExclusionOrConstraints) {
+                _graphPresenter.changeState(element, ValidateStatus.Invalid);
+            }
+            List<ORM_ExclusionOrConstraint> exclusionOrConstraintList = _clientDiagramModel.getElements(ORM_ExclusionOrConstraint.class).
+                    filter(e -> _graphPresenter.getElementPresenter(e).getValidateStatus() == ValidateStatus.Invalid
+                            && e.getValidateStatus() == ValidateStatus.Acceptable).collect(Collectors.toList());
+            for (ORM_ExclusionOrConstraint element : exclusionOrConstraintList) {
+                _graphPresenter.changeState(element, ValidateStatus.Acceptable);
+            }
+        }
+
+        private void updateBinaryPredicatePresenter() {
+            // РЕДАКТИРОВАНИЕ ORM_UnaryPredicate
+            List<ORM_BinaryPredicate> binaryPredicates = _clientDiagramModel.getElements(ORM_BinaryPredicate.class).
+                    filter(s -> s.getValidateStatus() == ValidateStatus.Invalid).collect(Collectors.toList());
+            for (ORM_BinaryPredicate element : binaryPredicates) {
+                _graphPresenter.changeState(element, ValidateStatus.Invalid);
+            }
+            List<ORM_BinaryPredicate> binaryPredicateList = _clientDiagramModel.getElements(ORM_BinaryPredicate.class).
+                    filter(e -> _graphPresenter.getElementPresenter(e).getValidateStatus() == ValidateStatus.Invalid
+                            && e.getValidateStatus() == ValidateStatus.Acceptable).collect(Collectors.toList());
+            for (ORM_BinaryPredicate element : binaryPredicateList) {
+                _graphPresenter.changeState(element, ValidateStatus.Acceptable);
+            }
+        }
+
+        private void updateInclusiveOrConstraintPresenter() {
+            // РЕДАКТИРОВАНИЕ ORM_InclusiveOrConstraint
+            List<ORM_InclusiveOrConstraint> ormInclusiveOrConstraints = _clientDiagramModel.getElements(ORM_InclusiveOrConstraint.class).
+                    filter(s -> s.getValidateStatus() == ValidateStatus.Intermediate).collect(Collectors.toList());
+            for (ORM_InclusiveOrConstraint element : ormInclusiveOrConstraints) {
+                _graphPresenter.changeState(element, ValidateStatus.Invalid);
+            }
+            List<ORM_InclusiveOrConstraint> inclusiveOrConstraintList = _clientDiagramModel.getElements(ORM_InclusiveOrConstraint.class).
+                    filter(e -> _graphPresenter.getElementPresenter(e).getValidateStatus() == ValidateStatus.Invalid
+                            && e.getValidateStatus() == ValidateStatus.Acceptable).collect(Collectors.toList());
+            for (ORM_InclusiveOrConstraint element : inclusiveOrConstraintList) {
+                _graphPresenter.changeState(element, ValidateStatus.Acceptable);
+            }
+        }
+
+        private void updateUnaryPredicatePresenter() {
+            // РЕДАКТИРОВАНИЕ ORM_UnaryPredicate
+            List<ORM_UnaryPredicate> unaryPredicates = _clientDiagramModel.getElements(ORM_UnaryPredicate.class).
+                    filter(s -> s.getValidateStatus() == ValidateStatus.Invalid).collect(Collectors.toList());
+            for (ORM_UnaryPredicate element : unaryPredicates) {
+                _graphPresenter.changeState(element, ValidateStatus.Invalid);
+            }
+            List<ORM_UnaryPredicate> unaryPredicateList = _clientDiagramModel.getElements(ORM_UnaryPredicate.class).
+                    filter(e -> _graphPresenter.getElementPresenter(e).getValidateStatus() == ValidateStatus.Invalid
+                            && e.getValidateStatus() == ValidateStatus.Acceptable).collect(Collectors.toList());
+            for (ORM_UnaryPredicate element : unaryPredicateList) {
+                _graphPresenter.changeState(element, ValidateStatus.Acceptable);
+            }
+        }
+
+        private void updateValueTypePresenter() {
+            // РЕДАКТИРОВАНИЕ ORM_ValueType
+            List<ORM_ValueType> valueTypes = _clientDiagramModel.getElements(ORM_ValueType.class).
+                    filter(s -> s.getValidateStatus() == ValidateStatus.Invalid).collect(Collectors.toList());
+            for (ORM_ValueType element : valueTypes) {
+                _graphPresenter.changeState(element, ValidateStatus.Invalid);
+            }
+            List<ORM_ValueType> diagramElements2 = _clientDiagramModel.getElements(ORM_ValueType.class).
+                    filter(e -> _graphPresenter.getElementPresenter(e).getValidateStatus() == ValidateStatus.Invalid
+                            && e.getValidateStatus() == ValidateStatus.Acceptable).collect(Collectors.toList());
+            for (ORM_ValueType element : diagramElements2) {
+                _graphPresenter.changeState(element, ValidateStatus.Acceptable);
+            }
+        }
+
+        private void updateEntityTypePresenter() {
+            // РЕДАКТИРОВАНИЕ ORM_EntityType
+            List<ORM_EntityType> entityTypes = _clientDiagramModel.getElements(ORM_EntityType.class).
+                    filter(s -> s.getValidateStatus() == ValidateStatus.Invalid).collect(Collectors.toList());
+            for (ORM_EntityType element : entityTypes) {
+                _graphPresenter.changeState(element, ValidateStatus.Invalid);
+            }
+            List<ORM_EntityType> diagramElements1 = _clientDiagramModel.getElements(ORM_EntityType.class).
+                    filter(e -> _graphPresenter.getElementPresenter(e).getValidateStatus() == ValidateStatus.Invalid
+                            && e.getValidateStatus() == ValidateStatus.Acceptable).collect(Collectors.toList());
+            for (ORM_EntityType element : diagramElements1) {
+                _graphPresenter.changeState(element, ValidateStatus.Acceptable);
+            }
+        }
+    }
 
     public static void main(String[] args) {
 
