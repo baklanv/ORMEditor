55969213a6deecfafee79df25316bfadba3f98cc
baklanv <63604180+baklanv@users.noreply.github.com>
2021-06-11 14:14:52 +0300

    Исправлено форматирование

--- src/ORM2Editor/ORM_mxMultiplicity.java
+++ src/ORM2Editor/ORM_mxMultiplicity.java
@@ -5,8 +5,6 @@ import com.mxgraph.model.mxCell;
 import com.mxgraph.view.mxGraph;
 import com.mxgraph.view.mxMultiplicity;
 
-import java.util.Collection;
-
 public class ORM_mxMultiplicity extends mxMultiplicity {
     GraphPresenter _graphPresenter;
 
@@ -20,15 +18,11 @@ public class ORM_mxMultiplicity extends mxMultiplicity {
     }
 
     @Override
-    public String check(mxGraph graph, Object edge, Object source,
-                        Object target, int sourceOut, int targetIn) {
-
+    public String check(mxGraph graph, Object edge, Object source, Object target, int sourceOut, int targetIn) {
         String result = "";
-
         if (source != null && target != null) {
             result = _graphPresenter.canEdgeConnected((mxCell) edge, (mxCell) source, (mxCell) target);
         }
-
         return result;
     }
 }
