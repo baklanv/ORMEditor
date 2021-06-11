3f33536a925977a4838e2761eba8832a233143a6
baklanv <63604180+baklanv@users.noreply.github.com>
2021-06-11 14:13:40 +0300

    Исправлено форматирование кода

--- src/ORM_Event/ORM_EventListener.java
+++ src/ORM_Event/ORM_EventListener.java
@@ -3,6 +3,7 @@ package ORM_Event;
 import java.util.EventListener;
 
 public interface ORM_EventListener extends EventListener {
-    public void edgeConnected(ORM_EventObject e);
-    public void processOfCreatingConstrainAssociation(ORM_EventObject e);
+    void edgeConnected(ORM_EventObject e);
+
+    void processOfCreatingConstrainAssociation(ORM_EventObject e);
 }
--- src/ORM_Event/ORM_EventObject.java
+++ src/ORM_Event/ORM_EventObject.java
@@ -5,19 +5,17 @@ import com.mxgraph.model.mxCell;
 import java.util.EventObject;
 
 public class ORM_EventObject extends EventObject {
-
     private mxCell _edge;
 
-    public void set_mxCell(mxCell edge){
+    public void set_mxCell(mxCell edge) {
         _edge = edge;
     }
 
-    public mxCell get_mxCell(){
+    public mxCell get_mxCell() {
         return _edge;
     }
 
     public ORM_EventObject(Object source) {
         super(source);
     }
-
 }
